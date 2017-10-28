86142f3b06c4c63927264595c158a252531814f2
Marcin Wolf <wolflint@users.noreply.github.com>
2017-10-28 13:47:12 +0100

    Fixed Index responsiveness

--- js/myScripts.js
+++ js/myScripts.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
     var movementStrength = 30;
     var height = movementStrength / $(window).height();
     var width = movementStrength / $(window).width();
-    $("#top-image").mousemove(function (e) {
+    $("body").mousemove(function (e) {
       var pageX = e.pageX - ($(window).width() / 2);
       var pageY = e.pageY - ($(window).height() / 2);
       var newvalueX = width * pageX * -1 - 25;
       var newvalueY = height * pageY * -1 - 50;
-      $('#top-image').css("background-position", newvalueX + "px     " + newvalueY + "px");
+      $('body').css("background-position", newvalueX + "px     " + newvalueY + "px");
     });
   });
