a7d13176e1011ab93812a9b4e1759faf3524b9a4
Marcin Wolf <wolflint@users.noreply.github.com>
2017-11-01 15:25:00 +0000

    Added script to allow animation on hover

--- js/myScripts.js
+++ js/myScripts.js
@@ -37,3 +37,22 @@ $(document).ready(function () {
     } // End if
   });
 });
+
+/* animationHover('#rune2', 'swing');
+animationHover('#rune3', 'swing');
+animationHover('#rune4', 'swing');
+animationHover('#rune1', 'swing'); */
+
+function animationHover(element, animation){
+  element = $(element);
+  element.hover(
+      function() {
+          element.addClass('animated ' + animation);        
+      },
+      function(){
+          //wait for animation to finish before removing classes
+          window.setTimeout( function(){
+              element.removeClass('animated ' + animation);
+          }, 2000);         
+      });
+}
