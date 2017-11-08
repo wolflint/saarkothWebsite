ba79131f366b24260e262b1d74d923719c1fd5c2
Marcin Wolf <wolflint@users.noreply.github.com>
2017-11-08 16:05:28 +0000

    Broke more things

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,14 +1,26 @@
-//Background image parralax
+// SMOOTH SCROLLING
 $(document).ready(function () {
-  var movementStrength = 30;
-  var height = movementStrength / $(window).height();
-  var width = movementStrength / $(window).width();
-  $("#parallax-section").mousemove(function (e) {
-    var pageX = e.pageX - ($(window).width() / 2);
-    var pageY = e.pageY - ($(window).height() / 2);
-    var newvalueX = width * pageX * -1 - 25;
-    var newvalueY = height * pageY * -1 - 50;
-    $('#parallax-section').css("background-position", newvalueX + "px     " + newvalueY + "px");
+  // Add smooth scrolling to all links
+  $(".smooth-scroll").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
   });
 });
 
