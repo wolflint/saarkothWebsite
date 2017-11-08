ae1b6d04e699b75204b7717c93e77ef98645fb2d
Marcin Wolf <marcinwolf363@gmail.com>
2017-11-08 23:29:43 +0000

    small changes to index

--- js/myScripts.js
+++ js/myScripts.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function () {
+      }, 500, function () {
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
@@ -26,7 +26,7 @@ $(document).ready(function () {
 
 // MOBILE COLLAPSIBLE MENU
 $(".button-collapse").sideNav({
-  menuWidth: 200,
+  menuWidth: 250,
   edge: 'right', 
   closeOnClick: true,
   draggable: true,
