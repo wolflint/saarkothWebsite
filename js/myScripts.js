b227c3ea8a99ccf57e69941cb24152ff6aafffe1
Marcin Wolf <marcinwolf363@gmail.com>
2017-11-16 00:24:20 +0000

    Moved index CSS back to index.css

--- js/myScripts.js
+++ js/myScripts.js
@@ -60,3 +60,18 @@ function animationHover(element, animation) {
   $(document).ready(function(){
     $('.materialboxed').materialbox();
   });
+
+//PARALLAX
+$(document).ready(function() {
+
+	var movementStrength = 25;
+	var height = movementStrength / $(window).height();
+	var width = movementStrength / $(window).width();
+	$("#parallax-section").mousemove(function(e) {
+		var pageX = e.pageX - ($(window).width() / 2);
+		var pageY = e.pageY - ($(window).height() / 2);
+		var newvalueX = width * pageX * -1 - 25;
+		var newvalueY = height * pageY * -1 - 50;
+		$('#parallax-section').css("background-position", newvalueX + "px     " + newvalueY + "px");
+	});
+});
