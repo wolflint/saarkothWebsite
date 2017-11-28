87cab1a771799935c527ae6f431bbebb9f5d197b
Marcin Wolf <marcinwolf363@gmail.com>
2017-11-28 22:22:32 +0000

    removed parallax on Y axis

--- js/myScripts.js
+++ js/myScripts.js
@@ -45,9 +45,7 @@ $(document).ready(function() {
 	var width = movementStrength / $(window).width();
 	$("#parallax-section").mousemove(function(e) {
 		var pageX = e.pageX - ($(window).width() / 2);
-		var pageY = e.pageY - ($(window).height() / 2);
 		var newvalueX = width * pageX * -1 - 25;
-		var newvalueY = height * pageY * -1 - 50;
-		$('#parallax-section').css("background-position", newvalueX + "px     " + newvalueY + "px");
+		$('#parallax-section').css("background-position", newvalueX + "px");
 	});
 });
