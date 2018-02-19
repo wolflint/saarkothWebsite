c0ab134d4e6c376e66f060079314e9c4f85c9388
Unknown <marcinwolf363@gmail.com>
2018-02-19 12:16:56 +0000

    Imporved dynamic nav

---
+++ about.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Saarkoth - About</title>
+	<link rel="stylesheet" href="css/myStyle.css" type="text/css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="icon" href="./assets/favicon.png">
+
+</head>
+
+<body>
+	<!-- HEADER -->
+	<?php include('includes/header.php') ?>
+	<!-- END HEADER -->
+
+	<main class="container">
+		<section id="band">
+			<div class="row valign-wrapper">
+				<div class="col l3">
+					<div class="flow-text">
+						<h1>Who are we?</h1>
+						<p>Saarkoth formed in 2016 when two friends began experimenting with similar musical ideas.</p>
+						<p>After writing a series of melodic, atmospheric and dynamic songs together, Saarkoth was formed and work on the bands
+							debut album 'Jera' went underway.</p>
+					</div>
+				</div>
+				<div class="col l9">
+					<img class="responsive-img" src="./assets/about/about.jpg">
+				</div>
+		</section>
+
+		<!-- BAND MEMBER CARDS -->
+		<section id="members">
+			<div class="row">
+				<div class="col s12 m12">
+					<h1 class="center">Members</h1>
+				</div>
+				<div class="col m4">
+					<img class="responsive-img center materialboxed" width="500" src="./assets/about/lewis.jpg" alt="">
+					<h4 class="center names">Lewis Baker</h4>
+					<p class="center">Lead Guitar</p>
+				</div>
+				<div class="col m4">
+					<img class="responsive-img center materialboxed" width="500" src="./assets/about/ellis.jpg" alt="">
+					<h4 class="center names">Ellis Arnott</h4>
+					<p class="center">Rhythm Guitar / Vocals</p>
+				</div>
+				<div class="col m4">
+					<img class="responsive-img center materialboxed" width="500" src="./assets/about/asa.jpg" alt="">
+					<h4 class="center names">Asa Jones</h4>
+					<p class="center">Bass / Vocals</p>
+				</div>
+
+		</section>
+	</main>
+<?php include('includes/footer.php'); ?>
--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 
 				<div>
 					<div class="col s12 m3">
-						<a class="runes" href="news.html">
+						<a class="runes" href="news.php">
 							<h1>ᚠᚱᛁᚴᚾ</h1>
 							<h6>News</h6>
 						</a>
@@ -38,7 +38,7 @@
 
 				<div>
 					<div class="col s12 m3">
-						<a class="runes" href="about.html">
+						<a class="runes" href="about.php">
 							<h1>ᛋᛒᛁᚱᛁᛅ</h1>
 							<h6>About</h6>
 						</a>
@@ -47,7 +47,7 @@
 
 				<div>
 					<div class="col s12 m3">
-						<a class="runes" href="music.html">
+						<a class="runes" href="music.php">
 							<h1>ᚼᛚᛅ</h1>
 							<h6>Music</h6>
 						</a>
@@ -56,7 +56,7 @@
 
 				<div>
 					<div class="col s12 m3">
-						<a class="runes" href="merch.html">
+						<a class="runes" href="merch.php">
 							<h1>ᚴᛅᚢᛒᛅ</h1>
 							<h6>Merchandise</h6>
 						</a>
@@ -141,52 +141,4 @@
 		</section>
 	</main>
 
-	<!-- FOOTER -->
-	<footer class="page-footer">
-			<div class="container">
-				<div class="row">
-					<div class="col l6 s12">
-						<img class="navbar-logo" style="opacity: 0.7" src="assets/logoMin.png" alt="">
-					</div>
-					<div class="col l4 offset-l2 s12">
-						<ul>
-							<li>
-								<a class="col s2" href="https://www.facebook.com/saarkoth/" target="_blank">
-									<i id="fb" class="fa fa-facebook-f fa-lg footer-social center"></i>
-								</a>
-							</li>
-							<li>
-								<a class="col s2" href="https://twitter.com/saarkoth" target="_blank">
-									<i id="tw" class="fa fa-twitter fa-lg footer-social center"></i>
-								</a>
-							</li>
-							<li>
-								<a class="col s2" href="https://soundcloud.com/user-985488728" target="_blank">
-									<i id="sc" class="fa fa-soundcloud fa-lg footer-social center"></i>
-								</a>
-							</li>
-							<li>
-								<a class="col s2" href="https://saarkothofficial.bandcamp.com/releases" target="_blank">
-									<i id="bc" class="fa fa-bandcamp fa-lg footer-social center"></i>
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="footer-copyright">
-				<div class="container">
-					<span style="font-family:Roboto;">©</span> 2017 Saarkoth
-				</div>
-			</div>
-		</footer>
-		<!-- END FOOTER -->
-
-	<!-- SCRIPTS -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="js/materialize.min.js"></script>
-	<script src="js/myScripts.js"></script>
-
-</body>
-
-</html>
+<?php include('includes/footer.php'); ?>
