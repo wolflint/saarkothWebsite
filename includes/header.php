17e516c1f5d6c6c72c113abbfc3c17277bdf1e24
wolflint <marcinwolf363@gmail.com>
2018-02-28 23:34:20 +0000

    Beautify

--- includes/header.php
+++ includes/header.php
@@ -1,48 +1,49 @@
 <header>
-		<!-- NAVIGATION -->
-		<nav>
-			<div class="nav-wrapper">
+    <!-- NAVIGATION -->
+    <nav>
+        <div class="nav-wrapper">
 
-				<!-- NAV BRAND-LOGO and MOBILE NAV-MENU BUTTON -->
-				<a href="index.php" class="brand-logo center">SAARKOTH</a>
-				<a href="#" data-activates="mobile-demo" class="right button-collapse">
+            <!-- NAV BRAND-LOGO and MOBILE NAV-MENU BUTTON -->
+            <a href="index.php" class="brand-logo center">SAARKOTH</a>
+            <a href="#" data-activates="mobile-demo" class="right button-collapse">
 					<i class="material-icons">menu</i>
 				</a>
 
-				<!-- LEFT OF BRAND-LOGO -->
-				<ul class="left hide-on-med-and-down">
-					<?php include('includes/nav.php'); ?>
-				</ul>
-				<!-- RIGHT OF BRAND-LOGO -->
-				<ul class="right hide-on-med-and-down">
-				<!-- INSERT LOGIN BUTTONS -->
-				<li><a class="waves-effect waves-light btn modal-trigger" href="#login">Login</a></li>
-				</ul>
+            <!-- LEFT OF BRAND-LOGO -->
+            <ul class="left hide-on-med-and-down">
+                <?php include('includes/nav.php'); ?>
+            </ul>
+            <!-- RIGHT OF BRAND-LOGO -->
+            <ul class="right hide-on-med-and-down">
+                <!-- INSERT LOGIN BUTTONS -->
+                <li><a class="waves-effect waves-light btn modal-trigger" href="#login">Login</a></li>
+            </ul>
 
-				<!-- MOBILE NAV-MENU -->
-				<ul class="side-nav" id="mobile-demo">
-				<?php include('includes/nav.php'); ?>
-				</ul>
+            <!-- MOBILE NAV-MENU -->
+            <ul class="side-nav" id="mobile-demo">
+                <?php include('includes/nav.php'); ?>
+            </ul>
 
-			</div>
-		</nav>
+        </div>
+    </nav>
 
-<!-- MODAL -->
-		<div id="login" class="modal">
-    <div class="modal-content">
-      <h4>Modal Header</h4>
-      <p>A bunch of text</p>
+    <!-- MODAL -->
+    <div id="login" class="modal">
+        <div class="modal-content">
+            <h4>Modal Header</h4>
+            <p>A bunch of text</p>
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+        </div>
     </div>
-    <div class="modal-footer">
-      <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
-    </div>
-  </div>
 
-	<script type="text/javascript">
-	$(document).ready(function(){
-// the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-$('.modal').modal();
-});
-	</script>
-		<!-- END NAVIGATION -->
-	</header>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+            $('.modal').modal();
+        });
+
+    </script>
+    <!-- END NAVIGATION -->
+</header>
--- music.php
+++ music.php
@@ -2,137 +2,139 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Saarkoth - Music</title>
-	<link rel="stylesheet" href="css/myStyle.css" type="text/css">
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<link rel="icon" href="./assets/favicon.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Saarkoth - Music</title>
+    <link rel="stylesheet" href="css/myStyle.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="icon" href="./assets/favicon.png">
 
 </head>
 
 <style>
+
+
 </style>
 
 <body>
-	<!-- HEADER -->
-	<?php include('includes/header.php') ?>
-	<!-- END HEADER -->
+    <!-- HEADER -->
+    <?php include('includes/header.php') ?>
+    <!-- END HEADER -->
 
-	<!-- SECTIONS -->
-	<main class="container">
+    <!-- SECTIONS -->
+    <main class="container">
 
-		<section id="digital">
-			<div class="row">
+        <section id="digital">
+            <div class="row">
 
-				<div class="col s12">
-					<!-- SECTION TITLE -->
-					<h1>DIGITAL</h1>
-				</div>
-				<!-- CARDS -->
-				<div class="col m4">
-					<div class="card">
-						<div class="card-image">
-							<img class="materialboxed" src="./assets/news/news3.jpg" alt="">
-							<a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
+                <div class="col s12">
+                    <!-- SECTION TITLE -->
+                    <h1>DIGITAL</h1>
+                </div>
+                <!-- CARDS -->
+                <div class="col m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="materialboxed" src="./assets/news/news3.jpg" alt="">
+                            <a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
 								<i class="material-icons">add_shopping_cart</i>
 							</a>
-						</div>
-						<div class="card-content grey darken-4">
-							<span class="card-title right">£0.99</span>
-							<p>BEYOND THE HORIZON</p>
-						</div>
-					</div>
-				</div>
+                        </div>
+                        <div class="card-content grey darken-4">
+                            <span class="card-title right">£0.99</span>
+                            <p>BEYOND THE HORIZON</p>
+                        </div>
+                    </div>
+                </div>
 
-				<div class="col m4">
-					<div class="card">
-						<div class="card-image">
-							<img class="materialboxed" src="./assets/news/news3.jpg" alt="">
-							<a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
+                <div class="col m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="materialboxed" src="./assets/news/news3.jpg" alt="">
+                            <a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
 								<i class="material-icons">add_shopping_cart</i>
 							</a>
-						</div>
-						<div class="card-content grey darken-4">
-							<span class="card-title right">£0.99</span>
-							<p>AWAKE IN ETERNAL SLEEP</p>
-						</div>
-					</div>
-				</div>
-				<div class="col m4">
+                        </div>
+                        <div class="card-content grey darken-4">
+                            <span class="card-title right">£0.99</span>
+                            <p>AWAKE IN ETERNAL SLEEP</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col m4">
 
-					<div class="card">
-						<div class="card-image">
-							<img class="materialboxed" src="./assets/news/news3.jpg" alt="">
-							<a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="materialboxed" src="./assets/news/news3.jpg" alt="">
+                            <a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
 								<i class="material-icons">add_shopping_cart</i>
 							</a>
-						</div>
-						<div class="card-content grey darken-4">
-							<span class="card-title right">£0.99</span>
-							<p>THE WONDERER</p>
-						</div>
-					</div>
-				</div>
-		</section>
+                        </div>
+                        <div class="card-content grey darken-4">
+                            <span class="card-title right">£0.99</span>
+                            <p>THE WONDERER</p>
+                        </div>
+                    </div>
+                </div>
+        </section>
 
-		<section id="vinyls">
-			<div class="row">
+        <section id="vinyls">
+            <div class="row">
 
-				<div class="col s12">
-					<!-- SECTION TITLE -->
-					<h1>VINYLS &amp; CD
-						<small>s</small>
-					</h1>
-				</div>
-				<!-- CARDS -->
-				<div class="col m4">
-					<div class="card">
-						<div class="card-image">
-							<img class="materialboxed" src="./assets/music/vinyl.png" alt="">
-							<a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
+                <div class="col s12">
+                    <!-- SECTION TITLE -->
+                    <h1>VINYLS &amp; CD
+                        <small>s</small>
+                    </h1>
+                </div>
+                <!-- CARDS -->
+                <div class="col m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="materialboxed" src="./assets/music/vinyl.png" alt="">
+                            <a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
 								<i class="material-icons">add_shopping_cart</i>
 							</a>
-						</div>
-						<div class="card-content grey darken-4">
-							<span class="card-title right">£11.99</span>
-							<p>BEYOND THE HORIZON (VINYL SINGLE)</p>
-						</div>
-					</div>
-				</div>
+                        </div>
+                        <div class="card-content grey darken-4">
+                            <span class="card-title right">£11.99</span>
+                            <p>BEYOND THE HORIZON (VINYL SINGLE)</p>
+                        </div>
+                    </div>
+                </div>
 
-				<div class="col m4">
-					<div class="card">
-						<div class="card-image">
-							<img class="materialboxed" src="./assets/music/vinyl.png" alt="">
-							<a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
+                <div class="col m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="materialboxed" src="./assets/music/vinyl.png" alt="">
+                            <a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
 								<i class="material-icons">add_shopping_cart</i>
 							</a>
-						</div>
-						<div class="card-content grey darken-4">
-							<span class="card-title right">£11.99</span>
-							<p>AWAKE IN ETERNAL SLEEP (VINYL SINGLE)</p>
-						</div>
-					</div>
-				</div>
+                        </div>
+                        <div class="card-content grey darken-4">
+                            <span class="card-title right">£11.99</span>
+                            <p>AWAKE IN ETERNAL SLEEP (VINYL SINGLE)</p>
+                        </div>
+                    </div>
+                </div>
 
-				<div class="col m4">
-					<div class="card">
-						<div class="card-image">
-							<img class="materialboxed" src="./assets/music/demo.png" alt="">
-							<a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
+                <div class="col m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="materialboxed" src="./assets/music/demo.png" alt="">
+                            <a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
 								<i class="material-icons">add_shopping_cart</i>
 							</a>
-						</div>
-						<div class="card-content grey darken-4">
-							<span class="card-title right">£11.99</span>
-							<p>SAARKOTH 2017 DEMO CD</p>
-						</div>
-					</div>
-				</div>
-		</section>
+                        </div>
+                        <div class="card-content grey darken-4">
+                            <span class="card-title right">£11.99</span>
+                            <p>SAARKOTH 2017 DEMO CD</p>
+                        </div>
+                    </div>
+                </div>
+        </section>
 
-	</main>
+    </main>
 
-<?php include('includes/footer.php'); ?>
+    <?php include('includes/footer.php'); ?>
