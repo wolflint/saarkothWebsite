ff92b36d905009374545f05d290ff416668a9e04
wolflint <marcinwolf363@gmail.com>
2018-03-14 18:56:18 +0000

    edit

--- includes/edit.inc.php
+++ includes/edit.inc.php
@@ -1,5 +1,4 @@
 <?php
-include_once 'login.inc.php';
 include_once 'dbh.inc.php';
 
 if (isset($_POST['submit'])) {
@@ -12,12 +11,14 @@ if (isset($_POST['submit'])) {
     $city = mysqli_real_escape_string($conn, $_POST['city']);
 
     $sql = "UPDATE users SET user_first='$first', user_last='$last', user_email='$email', user_address='$address', user_postcode='$postcode', user_city='$city' WHERE user_id='$seshID';";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
 
-    if (mysqli_query($conn, $sql) === TRUE) {
-    header("Location: ../account.php?edit=success");
+    if ($resultCheck > 0) {
+        header("Location: ../account.php?edit=success");
     }
     else {
         header("Location: ../account.php?edit=error");
     }
-    exit();
 }
+?>
