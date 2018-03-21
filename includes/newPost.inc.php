fb6044c2f43f9e32f0cae41520fd170f9e2be70d
wolflint <marcinwolf363@gmail.com>
2018-03-21 18:47:09 +0000

    news
    NDFOIUSND

--- includes/newPost.inc.php
+++ includes/newPost.inc.php
@@ -3,7 +3,7 @@ session_start();
 
 if (isset($_POST['submit']) && isset($_SESSION['u_uid']) && $_SESSION['u_uid'] == 'admin') {
     include_once 'dbh.inc.php';
-    
+
     $seshID = $_SESSION['u_id'];
     // Text variables
     $title = $_POST['p_title'];
@@ -34,7 +34,7 @@ if (isset($_POST['submit']) && isset($_SESSION['u_uid']) && $_SESSION['u_uid'] =
                 $sql = "INSERT INTO news (title, image, content, users_user_id, post_date) VALUES ('$title', '$fileDestination', '$content', (SELECT user_id FROM users WHERE user_uid = 'admin'), NOW());";
                 $result = mysqli_query($conn, $sql);
 
-                if ($result === true) {
+                if ($result = true) {
                     header("Location: ../newPost.php?=postsuccess");
                     exit();
                 } else {
--- news.php
+++ news.php
@@ -1,4 +1,4 @@
-<?php $pageTitle = "Saarkoth - News"; include_once 'includes/header.php';?>
+<?php $pageTitle = "Saarkoth - News"; include_once 'includes/header.php'; include_once 'includes/dbh.inc.php';?>
 
 <!-- SECTIONS -->
 <main class="container">
@@ -7,7 +7,28 @@ if (isset($_SESSION['u_uid'])) {
 	if ($_SESSION['u_uid'] == 'admin') {
 		echo "<a class=\"right\" href=\"newPost.php\">New Post</a>";
 	}
+	$sql = "SELECT * FROM news ORDER BY post_id DESC;";
+	$result = mysqli_query($conn, $sql);
+	$resultCheck = mysqli_num_rows($result);
+
+	if ($resultCheck > 0) {
+		while ($row = mysqli_fetch_assoc($result)) {
+			$p_id = $row['post_id'];
+			$p_title = htmlspecialchars($row['title']);
+			$p_img = $row['image'];
+			$p_content = htmlspecialchars($row['content']);
+			$p_user = $row['users_user_id'];
+			$p_date = $row['post_date'];
+
+			$news .= "<div><h1>$p_title</h1><p>$p_date</p><img src=\"$p_img\" alt=\"\"><p>$p_content</p></div>";
+		}
+		echo $news;
+	}
 }
+
+$test = "<img src=\"assets/logo.png\" alt=\"\">";
+$fixed = htmlspecialchars($test);
+echo $fixed;
 ?>
 </main>
 
