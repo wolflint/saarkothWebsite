689bd0481db1ab1793512d6fd495bfa04381ff0f
wolflint <marcinwolf363@gmail.com>
2018-03-23 22:03:48 +0000

    News

--- includes/e_news.inc.php
+++ includes/e_news.inc.php
@@ -4,6 +4,21 @@ if (isset($_POST['submit']) && isset($_SESSION['u_id'])) {
     if ($_SESSION['u_uid'] == 'admin') {
         include_once 'dbh.inc.php';
 
+        $p_id = mysqli_real_escape_string($conn, $_POST['p_id']);
+        $p_title = mysqli_real_escape_string($conn, $_POST['p_title']);
+        $p_content = mysqli_real_escape_string($conn, $_POST['p_content']);
+
+        $sql = "UPDATE news SET title='$p_title', content='$p_content' WHERE post_id=$p_id;";
+        // $result = mysqli_query($conn, $sql);
+
+        if (mysqli_query($conn, $sql)) {
+            header("Location: ../news.php?edit=success");
+            exit();
+        } else {
+            // header("Location: ../news.php?edit=fail");
+            echo "fail";
+            exit();
+        }
     } else {
         header('Location: ../news.php?edit_error');
         exit();
--- includes/edit.inc.php
+++ includes/edit.inc.php
@@ -13,14 +13,13 @@ if (isset($_POST['submit']) && isset($_SESSION['u_id'])) {
 
     $sql = "UPDATE users SET user_first='$first', user_last='$last', user_email='$email', user_address='$address', user_postcode='$postcode', user_city='$city' WHERE user_id=$seshID";
     // $sql = "UPDATE users SET user_first='$first' WHERE user_id=$seshID'";
-    $result = mysqli_query($conn, $sql);
-    $resultCheck = mysqli_num_rows($result);
+    // $result = mysqli_query($conn, $sql);
+    // $resultCheck = mysqli_num_rows($result);
 
-    if ($resultCheck > 0) {
+    if (mysqli_query($conn, $sql)) {
         header("Location: ../account.php?edit=success");
         exit();
-    }
-    else {
+    } else {
         header("Location: ../account.php?edit=error");
         exit();
     }
