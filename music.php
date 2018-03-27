255a0b61bb444dd2e62a3d5f2798106c6d6399ea
wolflint <marcinwolf363@gmail.com>
2018-03-28 00:29:35 +0100

    music

--- music.php
+++ music.php
@@ -1,124 +1,49 @@
 <?php
 	$pageTitle = "Saarkoth - Music";
-	include_once 'includes/header.php';
+	include_once 'includes/header.php'; include_once 'includes/dbh.inc.php' ?>
 
-	//select all music from database
-	$sql = "SELECT * FROM music ORDER BY album AND track_no;";
-	?>
 
     <!-- SECTIONS -->
     <main class="container">
-
-        <section id="digital">
             <div class="row">
 
                 <div class="col s12">
                     <!-- SECTION TITLE -->
-                    <h1>DIGITAL</h1>
+                    <h1>Songs</h1>
                 </div>
                 <!-- CARDS -->
-                <div class="col m4">
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="materialboxed" src="./assets/news/news3.jpg" alt="">
-                            <a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
-								<i class="material-icons">add_shopping_cart</i>
-							</a>
-                        </div>
-                        <div class="card-content grey darken-4">
-                            <span class="card-title right">£0.99</span>
-                            <p>BEYOND THE HORIZON</p>
-                        </div>
-                    </div>
-                </div>
+				<?php
+				//select all music from database
+				$sql = "SELECT * FROM music ORDER BY album AND track_no;";
+				$result = mysqli_query($conn, $sql);
+				$resultCheck = mysqli_num_rows($result);
 
-                <div class="col m4">
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="materialboxed" src="./assets/news/news3.jpg" alt="">
-                            <a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
-								<i class="material-icons">add_shopping_cart</i>
-							</a>
-                        </div>
-                        <div class="card-content grey darken-4">
-                            <span class="card-title right">£0.99</span>
-                            <p>AWAKE IN ETERNAL SLEEP</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col m4">
+				//if no results display error message
+				if ($resultCheck < 1) {
+					echo "No music to display";
+					//if there are results display all of them as cards
+				} elseif ($resultCheck > 0) {
+					while ($row = mysqli_fetch_assoc($result)) {
+						echo "<div class=\"col m4\">
+		                    <div class=\"card\">
+		                        <div class=\"card-image\">
+		                            <img class=\"materialboxed\" src=\"./assets/music/" . $row['cover'] . "\"alt=\"\">
+									</div>
+			                        <div class=\"card-content grey darken-4\">
+			                            <p>Title: " . $row['title'] . "</p>
+			                            <p>Track: " . $row['track_no'] . "</p>
+			                            <p>Album: " . $row['album'] . "</p>
+			                            <p>Artist: " . $row['artist'] . "</p>
+			                            <p>Year: " . $row['year'] . "</p>
+			                            <p>Genre: " . $row['genre'] . "</p>
+			                            <p>Length: " . $row['track_time'] . "</p>
+			                        </div>
+			                    </div>
+			                </div>";
 
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="materialboxed" src="./assets/news/news3.jpg" alt="">
-                            <a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
-								<i class="material-icons">add_shopping_cart</i>
-							</a>
-                        </div>
-                        <div class="card-content grey darken-4">
-                            <span class="card-title right">£0.99</span>
-                            <p>THE WONDERER</p>
-                        </div>
-                    </div>
-                </div>
-        </section>
-
-        <section id="vinyls">
-            <div class="row">
-
-                <div class="col s12">
-                    <!-- SECTION TITLE -->
-                    <h1>VINYLS &amp; CD
-                        <small>s</small>
-                    </h1>
-                </div>
-                <!-- CARDS -->
-                <div class="col m4">
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="materialboxed" src="./assets/music/vinyl.png" alt="">
-                            <a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
-								<i class="material-icons">add_shopping_cart</i>
-							</a>
-                        </div>
-                        <div class="card-content grey darken-4">
-                            <span class="card-title right">£11.99</span>
-                            <p>BEYOND THE HORIZON (VINYL SINGLE)</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col m4">
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="materialboxed" src="./assets/music/vinyl.png" alt="">
-                            <a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
-								<i class="material-icons">add_shopping_cart</i>
-							</a>
-                        </div>
-                        <div class="card-content grey darken-4">
-                            <span class="card-title right">£11.99</span>
-                            <p>AWAKE IN ETERNAL SLEEP (VINYL SINGLE)</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col m4">
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="materialboxed" src="./assets/music/demo.png" alt="">
-                            <a class="btn-floating halfway-fab waves-effect waves-light  cyan darken-3">
-								<i class="material-icons">add_shopping_cart</i>
-							</a>
-                        </div>
-                        <div class="card-content grey darken-4">
-                            <span class="card-title right">£11.99</span>
-                            <p>SAARKOTH 2017 DEMO CD</p>
-                        </div>
-                    </div>
-                </div>
-        </section>
-
+					}
+				}
+				?>
     </main>
 
     <?php include('includes/footer.php'); ?>
